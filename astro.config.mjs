be49acb46b29217c508dd992631735ebcb3aa05e
0p3r4t0r <robert83sutherland@gmail.com>
2023-04-15 15:26:28 +0900

    poc: svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import vue from "@astrojs/vue";
-
 import solidJs from "@astrojs/solid-js";
+
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), vue(), solidJs()]
+  integrations: [react(), vue(), solidJs(), svelte()]
 });
