d605f8fde38b284997ba865bb9f914a7e86f2e10
0p3r4t0r <robert83sutherland@gmail.com>
2023-04-15 16:07:20 +0900

    poc: Angular

---
+++ src/components/MyAngularComponent.compontent.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "my-angular-component",
+  standalone: true,
+  template: ` <div>This is my angular component</div> `,
+})
+export class MyAngularComponent {}
