965c4cbc4257eea51f98bd0b92aff5a92e86d4ca
0p3r4t0r <robert83sutherland@gmail.com>
2023-04-15 21:15:13 +0900

    fix: ts astro config, disable angular due to errors

--- astro.config.ts
+++ astro.config.ts
@@ -1,12 +1,14 @@
-import { defineConfig } from 'astro/config';
+// https://docs.astro.build/en/guides/configuring-astro/#the-astro-config-file
+import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import vue from "@astrojs/vue";
 import solidJs from "@astrojs/solid-js";
 import svelte from "@astrojs/svelte";
 
+// TODO: Disabled angular due to errors with tsx files
 import analogjsangular from "@analogjs/astro-angular";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), vue(), solidJs(), svelte(), analogjsangular()]
+  integrations: [react(), vue(), solidJs(), svelte() /*analogjsangular()*/],
 });
